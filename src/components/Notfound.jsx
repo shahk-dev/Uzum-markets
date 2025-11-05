debe5924a43c62708c245be1e548fc42175e9d23
𝕾𝖍𝖆𝖝𝖗𝖎𝖞𝖔𝖗 𝕿𝖚𝖗𝖌'𝖚𝖓𝖔𝖛 <shaxriyorturgunov000@gmail.com>
2025-11-05 12:22:24 +0500

    Searching

--- src/components/Notfound.jsx
+++ src/components/Notfound.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Notfound() {
   return (
-    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque saepe, impedit ipsam eligendi commodi doloremque numquam sapiente accusantium officiis voluptas dolor quaerat natus fugiat quae suscipit sit officia quos ut.</div>
+      <div>
+          
+      </div>
   )
 }
 
