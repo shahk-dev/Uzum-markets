05b4d977f8b05a063f8713886a2a354c311fdc79
𝕾𝖍𝖆𝖝𝖗𝖎𝖞𝖔𝖗 𝕿𝖚𝖗𝖌'𝖚𝖓𝖔𝖛 <shaxriyorturgunov000@gmail.com>
2025-11-05 12:05:44 +0500

    Hi
    sa

--- src/components/Notfound.jsx
+++ src/components/Notfound.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Notfound() {
   return (
-    <div>Notfound</div>
+    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque saepe, impedit ipsam eligendi commodi doloremque numquam sapiente accusantium officiis voluptas dolor quaerat natus fugiat quae suscipit sit officia quos ut.</div>
   )
 }
 
